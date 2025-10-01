097d22bb31613f3e2157f3f8c7a5d5909ab1d4a2
JAESEUNGJAE <valkyrie9923@gmail.com>
2025-10-01 16:01:04 +0900

    feat: 이메일로 임시 비밀번호 전송 및 비밀번호 변경 기능 구현

--- src/main/java/com/pi/domain/user/user/service/EmailService.java
+++ src/main/java/com/pi/domain/user/user/service/EmailService.java
@@ -1,6 +1,7 @@
 package com.pi.domain.user.user.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -10,9 +11,14 @@ import org.springframework.stereotype.Service;
 public class EmailService {
     private final JavaMailSender mailSender;
 
+    @Value("${spring.mail.username}")
+    private String fromUsername;
+
     public void sendTemporaryPasswordEmail(String toEmail, String temporaryPassword) {
         SimpleMailMessage message = new SimpleMailMessage();
 
+        String fromAddress = fromUsername + "@naver.com";
+        message.setFrom(fromAddress);
         message.setTo(toEmail);
         message.setSubject("임시 비밀번호 안내");
         String text = String.format(
