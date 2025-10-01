5903f48a9f7f91d7ac3dddfdd2823d28eede6e43
leejiyeon <sky75013@naver.com>
2025-10-02 00:50:29 +0900

    fix: @NotNull검증 위해 freelancerId Long으로 변경

--- src/main/java/com/pi/domain/offer/offer/dto/OfferCreateReqBody.java
+++ src/main/java/com/pi/domain/offer/offer/dto/OfferCreateReqBody.java
@@ -1,6 +1,8 @@
 package com.pi.domain.offer.offer.dto;
 
+import jakarta.validation.constraints.NotNull;
+
 public record OfferCreateReqBody(
-        long freelancerId
+        @NotNull Long freelancerId
 ) {
 }
