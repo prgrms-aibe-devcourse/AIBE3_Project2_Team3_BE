ba09dd1a84c32c3b4cd0fc426154f61a92cb5905
ZeriTime <blee.940604@gmail.com>
2025-10-02 00:30:46 +0900

    fix:Answer entity 상속 변경

--- src/main/java/com/pi/domain/answer/answer/entity/Answer.java
+++ src/main/java/com/pi/domain/answer/answer/entity/Answer.java
@@ -5,11 +5,13 @@ import com.pi.domain.user.user.entity.User;
 import com.pi.global.jpa.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "answers")
