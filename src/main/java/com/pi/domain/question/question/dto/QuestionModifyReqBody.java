8e40a3673d8165fc90905eca5dec5e7b0fcdc365
ZeriTime <blee.940604@gmail.com>
2025-10-02 18:40:09 +0900

    feat: 문의 수정 dto

--- src/main/java/com/pi/domain/question/question/dto/QuestionModifyReqBody.java
+++ src/main/java/com/pi/domain/question/question/dto/QuestionModifyReqBody.java
@@ -3,7 +3,7 @@ package com.pi.domain.question.question.dto;
 import jakarta.validation.constraints.NotBlank;
 
 public record QuestionModifyReqBody (
-        @NotBlank(message = "제목을 수정해주세요")
+        @NotBlank(message = "수정항 제목을 입력해주세요")
         String title,
         @NotBlank(message = "수정할 내용을 입력해주세요")
         String content
