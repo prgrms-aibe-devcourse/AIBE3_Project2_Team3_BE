7f2e94440bdfe5d472837c3dc05f1b8925d0e949
luaerix <luaerix@naver.com>
2025-10-05 04:50:50 +0900

    fix: service

--- src/main/java/com/pi/domain/post/freelancer/service/FreelancerService.java
+++ src/main/java/com/pi/domain/post/freelancer/service/FreelancerService.java
@@ -18,43 +18,43 @@ public class FreelancerService {
     private final FreelancerRepository freelancerRepository;
     private final PostRepository postRepository;
 
-    @Transactional
-    public RsData<Freelancer> createFreelancer(User user, FreelancerReqBody reqBody) {
-        Post post = new Post(
-                user,
-                false,
-                reqBody.title(),
-                reqBody.content()
-        );
-        postRepository.save(post);
-
-        Freelancer freelancer = new Freelancer(post, reqBody.salary(), reqBody.period());
-        freelancerRepository.save(freelancer);
-
-        return new RsData<>("200-1", "프리랜서 게시글이 등록되었습니다.", freelancer);
-    }
-
-    @Transactional
-    public RsData<Freelancer> updateFreelancer(Long freelancerId, FreelancerReqBody reqBody, User user) {
-        Freelancer freelancer = freelancerRepository.findById(freelancerId)
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 프리랜서 게시글입니다."));
-
-        Post post = freelancer.getPost();
-
-        if(!post.getUser().equals(user)) {
-            return new RsData<>("403-1", "본인 게시글만 수정할 수 있습니다.");
-        }
-
-        post.setTitle(reqBody.title());
-        post.setContent(reqBody.content());
-        postRepository.save(post);
-        
-        freelancer.setSalary(reqBody.salary());
-        freelancer.setPeriod(reqBody.period());
-        freelancerRepository.save(freelancer);
-
-        return new RsData<>("200-2", "프리랜서 게시글이 수정되었습니다.", freelancer);
-    }
+//    @Transactional
+//    public RsData<Freelancer> createFreelancer(User user, FreelancerReqBody reqBody) {
+//        Post post = new Post(
+//                user,
+//                false,
+//                reqBody.title(),
+//                reqBody.content()
+//        );
+//        postRepository.save(post);
+//
+//        Freelancer freelancer = new Freelancer(post, reqBody.salary(), reqBody.period());
+//        freelancerRepository.save(freelancer);
+//
+//        return new RsData<>("200-1", "프리랜서 게시글이 등록되었습니다.", freelancer);
+//    }
+//
+//    @Transactional
+//    public RsData<Freelancer> updateFreelancer(Long freelancerId, FreelancerReqBody reqBody, User user) {
+//        Freelancer freelancer = freelancerRepository.findById(freelancerId)
+//                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 프리랜서 게시글입니다."));
+//
+//        Post post = freelancer.getPost();
+//
+//        if(!post.getUser().equals(user)) {
+//            return new RsData<>("403-1", "본인 게시글만 수정할 수 있습니다.");
+//        }
+//
+//        post.setTitle(reqBody.title());
+//        post.setContent(reqBody.content());
+//        postRepository.save(post);
+//
+//        freelancer.setSalary(reqBody.salary());
+//        freelancer.setPeriod(reqBody.period());
+//        freelancerRepository.save(freelancer);
+//
+//        return new RsData<>("200-2", "프리랜서 게시글이 수정되었습니다.", freelancer);
+//    }
 
 
 
