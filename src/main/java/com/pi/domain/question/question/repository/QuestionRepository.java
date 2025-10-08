3dcbf5de2f7006d8a83cc6fc8907c7fef318a190
ZeriTime <blee.940604@gmail.com>
2025-10-09 01:29:42 +0900

    refactor: query문 수정

--- src/main/java/com/pi/domain/question/question/repository/QuestionRepository.java
+++ src/main/java/com/pi/domain/question/question/repository/QuestionRepository.java
@@ -10,20 +10,26 @@ import java.util.List;
 
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
-    @Query("SELECT q FROM Question q")
+    @Query("""
+        SELECT q FROM Question q
+        """)
     Page<Question> findAll(Pageable pageable);
 
-    @Query("SELECT DISTINCT q FROM Question q " +
-            "LEFT JOIN FETCH q.answers a " +
-            "LEFT JOIN FETCH q.user qu " +
-            "LEFT JOIN FETCH a.user au " +
-            "WHERE q.id IN :ids")
+    @Query("""
+        SELECT DISTINCT q FROM Question q
+        LEFT JOIN FETCH q.answers a
+        LEFT JOIN FETCH q.user qu
+        LEFT JOIN FETCH a.user au
+        WHERE q.id IN :ids
+        """)
     List<Question> findAllWithAnswersByIds(List<Long> ids);
 
-    @Query("SELECT DISTINCT q FROM Question q " +
-            "LEFT JOIN FETCH q.answers a " +
-            "LEFT JOIN FETCH q.user qu " +
-            "LEFT JOIN FETCH a.user au " +
-            "WHERE q.user.id = :userId")
+    @Query("""
+        SELECT DISTINCT q FROM Question q
+        LEFT JOIN FETCH q.answers a
+        LEFT JOIN FETCH q.user qu
+        LEFT JOIN FETCH a.user au
+        WHERE q.user.id = :userId
+        """)
     List<Question> findAllWithAnswersByUserId(Long userId, Pageable pageable);
 }
