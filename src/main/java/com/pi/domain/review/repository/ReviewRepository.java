7e2e484bf504642eac59ecdfdef0237df95f1f43
luaerix <luaerix@naver.com>
2025-10-10 11:03:35 +0900

    feat: repository add

---
+++ src/main/java/com/pi/domain/review/repository/ReviewRepository.java
@@ -0,0 +1,10 @@
+package com.pi.domain.review.repository;
+
+import com.pi.domain.review.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+}
