7bfbb6b40ad339285bbf60c9e9b041206a1f2dcb
luaerix <luaerix@naver.com>
2025-10-10 10:52:38 +0900

    feat: review entity

---
+++ src/main/java/com/pi/domain/review/entity/Review.java
@@ -0,0 +1,28 @@
+package com.pi.domain.review.entity;
+
+import com.pi.domain.user.user.entity.User;
+import com.pi.global.jpa.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Review extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @Column(nullable = false)
+    private Integer rating;
+
+    @Column(columnDefinition ="TEXT", nullable = false)
+    private String comment;
+
+    public Review(User user, Integer rating, String comment) {
+        this.user = user;
+        this.rating = rating;
+        this.comment = comment;
+    }
+}
