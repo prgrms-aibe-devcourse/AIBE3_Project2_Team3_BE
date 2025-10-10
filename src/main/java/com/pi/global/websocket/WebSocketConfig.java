8c815c2d08bf20c561c82fd14f100baaced94ce4
JAESEUNGJAE <valkyrie9923@gmail.com>
2025-10-10 09:54:54 +0900

    refactor: WebSocketConfig 위치 옮김

--- src/main/java/com/pi/global/websocket/WebSocketConfig.java
+++ src/main/java/com/pi/global/websocket/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package com.pi.global.security;
+package com.pi.global.websocket;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
