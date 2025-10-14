bc019df6b300c37d8e419c933fd5ba09006ed392
luaerix <luaerix@naver.com>
2025-10-14 17:53:48 +0900

    feat: my freelancer repository add

--- src/main/java/com/pi/domain/post/freelancer/repository/FreelancerRepository.java
+++ src/main/java/com/pi/domain/post/freelancer/repository/FreelancerRepository.java
@@ -1,9 +1,11 @@
 package com.pi.domain.post.freelancer.repository;
 
 import com.pi.domain.post.freelancer.entity.Freelancer;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FreelancerRepository extends JpaRepository<Freelancer, Long> {
+    Page<Freelancer> findAllByPost_User(User user, Pageable pageable);
 }
