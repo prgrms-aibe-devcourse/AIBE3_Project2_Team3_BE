bbab31d78579a893eeea2122e104763175c50db5
luaerix <luaerix@naver.com>
2025-10-14 18:00:11 +0900

    feat: my freelancer page

--- src/main/java/com/pi/domain/post/freelancer/repository/FreelancerRepository.java
+++ src/main/java/com/pi/domain/post/freelancer/repository/FreelancerRepository.java
@@ -1,7 +1,9 @@
 package com.pi.domain.post.freelancer.repository;
 
 import com.pi.domain.post.freelancer.entity.Freelancer;
+import com.pi.domain.user.user.entity.User;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
