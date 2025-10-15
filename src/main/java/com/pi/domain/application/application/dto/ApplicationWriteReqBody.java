decd75e27d0e36c75aa0ed71c9f9f1a171b5ee09
leejiyeon <sky75013@naver.com>
2025-10-15 10:46:21 +0900

    fix: 구직 등록 요청 content 유효성 체크 수정

--- src/main/java/com/pi/domain/application/application/dto/ApplicationWriteReqBody.java
+++ src/main/java/com/pi/domain/application/application/dto/ApplicationWriteReqBody.java
@@ -2,10 +2,11 @@ package com.pi.domain.application.application.dto;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record ApplicationWriteReqBody(
         @NotNull Long postId,
-        @NotNull @Min(50) String content,
+        @NotNull @Size(min = 50) String content,
         @NotNull @Min(0) Long salary,
         @NotNull @Min(0) Integer period
 ) {
