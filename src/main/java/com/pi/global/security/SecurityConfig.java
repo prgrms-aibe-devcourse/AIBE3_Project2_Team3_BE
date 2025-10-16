7e502cfa8b3178dac859ce66275ed47499c0db9c
Yoepee <kdy2975@naver.com>
2025-10-17 01:14:48 +0900

    [chore] workflow 변경

--- src/main/java/com/pi/global/security/SecurityConfig.java
+++ src/main/java/com/pi/global/security/SecurityConfig.java
@@ -99,7 +99,8 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         // 허용할 오리진 설정
-        configuration.setAllowedOrigins(List.of("https://cdpn.io", "http://localhost:3000", "https://jobpick-one.vercel.app"));
+        configuration.setAllowedOrigins(List.of("https://cdpn.io", "http://localhost:3000", "https://jobpick-one.vercel.app", "https://jobpick.store",
+                "https://www.jobpick.store"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
 
         // 자격 증명 허용 설정
